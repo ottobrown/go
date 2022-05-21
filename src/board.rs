e3f274b6b655fc2e90a8675a3b624a5d4276bd32
ottobrown <henrynlyman@gmail.com>
2022-05-21 11:54:00 -0600

    add group finding algorithm

--- src/board.rs
+++ src/board.rs
@@ -1,6 +1,6 @@
 use std::collections::HashSet;
 
-#[derive(Clone, Copy, PartialEq)]
+#[derive(Clone, Copy, PartialEq, Debug)]
 pub enum Stone {
     Empty = 0,
     Black = 1,
@@ -31,7 +31,7 @@ impl Board {
     }
 
     pub fn height(&self) -> usize {
-        self.w
+        self.h
     }
 
     fn index(&self, x: usize, y: usize) -> usize {
@@ -39,7 +39,7 @@ impl Board {
     }
 
     pub fn get(&self, x: usize, y: usize) -> Option<Stone> {
-        if self.index(x, y) > self.stones.len() {
+        if self.index(x, y) >= self.stones.len() {
             return None;
         }
 
@@ -64,30 +64,112 @@ impl Board {
         }
 
         self.stones[index] = s;
+        self.add_group(x, y);
+
         return true
     }
 
     pub fn get_neighbors(&self, x: usize, y: usize) -> Neighbors {
-        let mut neighbors = Neighbors::default();
+        let c = self.neighbor_coords(x, y);
 
-        if x == 0 {
-            neighbors.left = None;
-        } else {
-            neighbors.left = self.get(x-1, y);
+        fn get(b: &Board, c: Option<(usize, usize)>) -> Option<Stone> {
+            if let Some(p) = c {
+                return b.get(p.0, p.1)
+            } else {
+                return None
+            }
         }
 
-        if y == 0  {
-            neighbors.up = None;
-        } else {
-            neighbors.up = self.get(x, y-1);
+        Neighbors {
+            up: get(self, c.up),
+            down: get(self, c.down),
+            left: get(self, c.left),
+            right: get(self, c.right),
         }
-
-        neighbors.right = self.get(x+1, y);
-        neighbors.down = self.get(x, y+1);
-
-        return neighbors
     }
 
+    pub fn neighbor_coords(&self, x: usize, y: usize) -> NeighborCoords {
+        let mut coords = NeighborCoords::default();
+        if x == 0 {
+            coords.left = None;
+        } else {
+            coords.left = Some((x-1, y));
+        }
+
+        if x > self.stones.len() {
+            coords.right = None;
+        } else {
+            coords.right = Some((x+1, y));
+        }
+
+        if y == 0 {
+            coords.up = None;
+        } else {
+            coords.up = Some((x, y-1));
+        }
+
+        if y > self.stones.len() {
+            coords.down = None;
+        } else {
+            coords.down = Some((x, y+1));
+        }
+
+        return coords;
+    }
+
+    pub fn add_group(&mut self, x: usize, y: usize) {
+        let color = match self.get(x, y) {
+            Some(c) => c,
+            None => {return},
+        };
+        let mut group = Group {
+            color: color,
+            points: HashSet::new(),
+            liberties: HashSet::new(),
+            enemy_neighbors: HashSet::new(),
+        };
+
+        fn add_neighbors(board: &Board, group: &mut Group, x: usize, y: usize) {
+            let neighbors = board.get_neighbors(x, y);
+            let coords = board.neighbor_coords(x, y);
+
+            let stone_array = [neighbors.up, neighbors.down, neighbors.left, neighbors.right];
+            let coord_array = [coords.up, coords.down, coords.left, coords.right];
+            
+            group.points.insert((x, y));
+            for i in 0..4 {
+                // If stone_array[i] is Some, coord_array[i] is Some
+                if let Some(s) = stone_array[i] {
+                    let c = coord_array[i].unwrap();
+
+                    if group.categorized(c.0, c.1) {
+                        continue;
+                    }
+
+                    if s == group.color {
+                        add_neighbors(board, group, c.0, c.1);
+                    } else if s == Stone::Empty {
+                        group.liberties.insert(c);
+                    } else {
+                        group.enemy_neighbors.insert(c);
+                    }
+                    // TODO: 'eat' nother group if they border
+                }
+            }
+        }
+
+        add_neighbors(&self, &mut group, x, y);
+
+        self.groups.push(group);
+    }
+}
+
+#[derive(Clone, Copy, Default)]
+pub struct NeighborCoords {
+    pub up: Option<(usize, usize)>,
+    pub down: Option<(usize, usize)>,
+    pub left: Option<(usize, usize)>,
+    pub right: Option<(usize, usize)>,
 }
 
 #[derive(Clone, Copy, Default)]
@@ -98,7 +180,7 @@ pub struct Neighbors {
     pub right: Option<Stone>,
 }
 
-#[derive(Clone)]
+#[derive(Clone, Debug)]
 pub struct Group {
     pub color: Stone,
     pub points: HashSet<(usize, usize)>,
