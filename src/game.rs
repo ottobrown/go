356eb76f125e79c6ecc26b9fa34fbcf38e28de4d
ottobrown <henrynlyman@gmail.com>
2022-05-27 01:02:34 -0600

    player names and ranks

--- src/game.rs
+++ src/game.rs
@@ -13,6 +13,12 @@ pub enum Event {
     Place(Stone, usize, usize),
 }
 
+#[derive(Clone, Copy)]
+pub enum Rank {
+    Kyu(u8),
+    Dan(u8),
+}
+
 #[derive(Clone)]
 pub struct Game {
     current_board: Board,
@@ -21,6 +27,11 @@ pub struct Game {
     turn: Stone,
     rules: Rules,
     end_game: Option<EndGame>,
+
+    pub black_player: String,
+    pub white_player: String,
+    pub black_rank: Option<Rank>,
+    pub white_rank: Option<Rank>,
 }
 impl Game {
     pub fn builder() -> NewGameBuilder {
@@ -56,16 +67,26 @@ impl Game {
 pub struct NewGameBuilder {
     pub size: (usize, usize),
     pub rules: Rules,
+
+    pub black_player: String,
+    pub white_player: String,
+    pub black_rank: Option<Rank>,
+    pub white_rank: Option<Rank>,
 }
 impl NewGameBuilder {
     pub fn build(&self) -> Game {
         Game {
             current_board: Board::blank(self.size.0, self.size.1),
             history: Vec::new(),
+
             turn: Stone::Black,
             rules: self.rules,
-
             end_game: None,
+
+            black_player: self.black_player.clone(),
+            white_player: self.white_player.clone(),
+            black_rank: self.black_rank,
+            white_rank: self.white_rank,
         }
     }
 }
@@ -75,6 +96,11 @@ impl Default for NewGameBuilder {
         Self {
             size: (19, 19),
             rules: Rules::CHINESE,
+
+            black_player: String::from("Black"),
+            white_player: String::from("White"),
+            black_rank: None,
+            white_rank: None,
         }
     }
 }
