af5fa48b75de3f458d8c00f4a90d2468b820bcd2
ottobrown <henrynlyman@gmail.com>
2023-08-03 23:19:41 -0600

    parse lists of values to a prop

--- src/sgf/action.rs
+++ src/sgf/action.rs
@@ -12,6 +12,7 @@ pub enum Action {
     PassWhite,
     Size(usize, usize),
     Other(String, String),
+    OtherMany(String, Vec<String>),
 }
 impl Action {
     #[allow(clippy::wrong_self_convention)]
@@ -34,6 +35,14 @@ impl Action {
                 }
             }
             Other(k, v) => format!("{}[{}]", &k, &v),
+            OtherMany(k, v) => {
+                let mut string = k.to_string();
+                for i in v {
+                    string.push_str(i);
+                }
+
+                string
+            }
         };
 
         Ok(s)
@@ -87,6 +96,12 @@ impl Action {
         })
     }
 
+    // clippy wants `v` to be &[String] instead of &Vec<String>
+    #[allow(clippy::ptr_arg)]
+    pub fn from_many(k: &str, v: &Vec<String>) -> SgfResult<Self> {
+        Ok(Action::OtherMany(k.to_string(), v.clone()))
+    }
+
     pub fn other(k: &str, v: &str) -> Self {
         Self::Other(String::from(k), String::from(v))
     }
@@ -98,32 +113,86 @@ impl std::fmt::Display for Action {
     }
 }
 
+#[derive(Clone, Debug, PartialEq)]
+enum PropFragment {
+    /// Out of []
+    Name(String),
+    /// Within []
+    Value(String),
+}
+
+fn to_fragments(s: &str) -> Vec<PropFragment> {
+    let mut fragments = Vec::new();
+
+    let mut buffer = String::new();
+
+    for c in s.chars() {
+        match c {
+            ';' => {}
+            '[' => {
+                if !buffer.is_empty() {
+                    fragments.push(PropFragment::Name(buffer.clone()));
+                    buffer.clear();
+                }
+            }
+            ']' => {
+                fragments.push(PropFragment::Value(buffer.clone()));
+                buffer.clear();
+            }
+
+            _ => buffer.push(c),
+        }
+    }
+
+    fragments
+}
+
 pub fn to_actions(s: &str) -> Vec<Action> {
     let mut actions = Vec::new();
 
-    // split the sgf text at every opening and closing bracket.
-    // this alternates between prop names and prop values
-    let mut split = s.split(['[', ']']);
+    let fragments = to_fragments(s);
 
-    while let (Some(l), Some(r)) = (split.next(), split.next()) {
-        let k = l.trim().trim_matches(';');
-        let v = r.trim().trim_matches(';');
+    let mut name = String::new();
+    let mut props: Vec<String> = Vec::new();
 
-        match Action::from_pair(k, v) {
-            Ok(a) => actions.push(a),
-            Err(e) => {
-                #[cfg(debug_assertions)]
-                crate::log(&format!(
-                    "[WARNING] Action::from_pair failed with {:?} on {}[{}] {} {}:{} ",
-                    e,
-                    k,
-                    v,
-                    file!(),
-                    line!(),
-                    column!()
-                ));
-                actions.push(Action::other(k, v))
+    for fragment in fragments {
+        match fragment {
+            PropFragment::Name(n) => {
+                if props.len() == 1 {
+                    let a = Action::from_pair(&name, &props[0]);
+                    match a {
+                        Ok(i) => actions.push(i),
+                        Err(_e) => crate::log("[WARNING] Action::from_pair failed"),
+                    };
+                }
+                if props.len() > 1 {
+                    let a = Action::from_many(&name, &props);
+                    match a {
+                        Ok(i) => actions.push(i),
+                        Err(_e) => crate::log("[WARNING] Action::from_many failed"),
+                    };
+                }
+
+                props.clear();
+                name = n;
             }
+            PropFragment::Value(v) => props.push(v),
+        };
+    }
+
+    // TODO: try to avoid ugly repetition
+    if props.len() == 1 {
+        let a = Action::from_pair(&name, &props[0]);
+        match a {
+            Ok(i) => actions.push(i),
+            Err(_e) => crate::log("[WARNING] Action::from_pair failed"),
+        };
+    }
+    if props.len() > 1 {
+        let a = Action::from_many(&name, &props);
+        match a {
+            Ok(i) => actions.push(i),
+            Err(_e) => crate::log("[WARNING] Action::from_many failed"),
         };
     }
 
@@ -131,9 +200,31 @@ pub fn to_actions(s: &str) -> Vec<Action> {
 }
 
 #[test]
+fn to_frag_test() {
+    assert_eq!(
+        to_fragments(";AB[aa][bb]W[cc]"),
+        vec![
+            PropFragment::Name("AB".to_string()),
+            PropFragment::Value("aa".to_string()),
+            PropFragment::Value("bb".to_string()),
+            PropFragment::Name("W".to_string()),
+            PropFragment::Value("cc".to_string()),
+        ]
+    )
+}
+
+#[test]
 fn to_actions_test() {
     assert_eq!(
         to_actions(";B[aa]W[bb]"),
         vec![Action::PlayBlack(0, 0), Action::PlayWhite(1, 1)]
+    );
+
+    assert_eq!(
+        to_actions(";AB[aa][bb]W[cc]"),
+        vec![
+            Action::OtherMany("AB".to_string(), vec!["aa".to_string(), "bb".to_string()]),
+            Action::PlayWhite(2, 2)
+        ]
     );
 }
